ff490bd1d70b4d0d86a094c2cb020318f1507aaa
rkorzen <korzeniewski@gmail.com>
2026-03-08 14:19:03 +0100

    modele

--- app/workshops/admin.py
+++ app/workshops/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Registration, Workshop
 
-# Register your models here.
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', "location")
+    search_fields = ('title', "location")
+    list_filter = ('start_date',)
+
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('workshop', 'name', 'email', 'created_at')
+    search_fields = ('workshop__title', 'name', 'email')
+    list_filter = ('workshop',)
--- app/workshops/models.py
+++ app/workshops/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    description = models.TextField(verbose_name=_('Description'))
+    start_date = models.DateTimeField(verbose_name=_('Start Date'))
+    location = models.CharField(max_length=255, verbose_name=_('Location'))
+    image = models.ImageField(
+        upload_to='workshops/',
+        blank=True,
+        null=True,
+        verbose_name=_('Image'))
+
+    class Meta:
+        ordering = ['start_date']
+        verbose_name = _('Workshop')
+        verbose_name_plural = _('Workshops')
+
+    def __str__(self):
+        return self.title
+
+class Registration(models.Model):
+    workshop = models.ForeignKey(
+        Workshop,
+        on_delete=models.CASCADE,
+        related_name='registrations',
+        verbose_name=_('Workshop')
+    )
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    email = models.EmailField(verbose_name=_('Email'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = _('Registration')
+        verbose_name_plural = _('Registrations')
+
+    def __str__(self):
+        return f"{self.name} -> {self.workshop.title}"
